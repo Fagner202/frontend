c38c4acc45f84545d390bb831ebd6f2b2dcfdda1
Fagner202 <fagnersilveira86@gmail.com>
2026-04-24 21:56:22 -0300

    Fix API configuration by correcting baseURL assignment and commenting out debug code in atendimentoService

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-    baseUrl: import.meta.env.VITE_API_URL,
-    // baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
